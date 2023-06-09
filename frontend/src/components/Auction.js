e0e5d3862bfa29f110972e2004365a83682ce330
Iamdevav <aviavipatel04@gmail.com>
2023-06-09 17:03:17 +0530

    seprate model

--- frontend/src/components/Auction.js
+++ frontend/src/components/Auction.js
@@ -9,29 +9,18 @@ import {
   Container,
 } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import {
-  createAuction,
-  createBid,
-  getAuction,
-  getBids,
-  login,
-} from "../action";
+import { createAuction, getAuction, getBids, login } from "../action";
 import "./style.css";
+import Bidder from "./Bidder";
 
 const Auction = () => {
   const [auctioneerName, setAuctioneerName] = useState("");
-  const [bidderName, setBidderName] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [itemName, setItemName] = useState("");
   const [startingPrice, setStartingPrice] = useState("");
   const [userType, setUserType] = useState();
-  const [showBidderModal, setShowBidderModal] = useState(false);
   const [auctions, setAuctions] = useState([]);
   const [bidders, setBidders] = useState([]);
-  const [bidAmount, setBidAmount] = useState();
-  const [addAmount, setAddAmount] = useState();
-
-  const currentUser = JSON.parse(localStorage.getItem("users"));
 
   const dispatch = useDispatch();
 
@@ -71,35 +60,6 @@ const Auction = () => {
   const handleModalClose = () => {
     setShowModal(false);
   };
-  const handleBidderSubmit = (event) => {
-    event.preventDefault();
-    if (bidderName) {
-      setUserType("Bidder");
-      const data = {
-        name: bidderName,
-        userType: "Biddder",
-      };
-      dispatch(login(data));
-      setShowBidderModal(true);
-      setBidAmount(
-        auctions.length > 0 && bidders.length === 0
-          ? parseInt(auctions[0].price) + 100
-          : bidders.length > 0 && bidders[bidders.length - 1].amount + 100
-      );
-    }
-  };
-  const handlebidderModalSubmit = (event, bidPrice) => {
-    event.preventDefault();
-    const data = {
-      auction_id: auctions.length > 0 && auctions[0].id,
-      amount: bidPrice,
-      name: bidderName,
-    };
-    dispatch(createBid(data));
-  };
-  const handlebidderModalClose = () => {
-    setShowBidderModal(false);
-  };
 
   return (
     <Container className="container-box">
@@ -130,28 +90,10 @@ const Auction = () => {
             </Card>
           </Col>
           <Col>
-            <Card>
-              <Card.Body>
-                <Card.Title>Bidder</Card.Title>
-                <Form onSubmit={handleBidderSubmit}>
-                  <Form.Group controlId="auctioneerName" className="mb-3">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter name"
-                      value={bidderName}
-                      onChange={(event) => setBidderName(event.target.value)}
-                    />
-                  </Form.Group>
-                  <Button variant="primary" type="submit">
-                    Submit
-                  </Button>
-                </Form>
-              </Card.Body>
-            </Card>
+            <Bidder />
           </Col>
         </Row>
-
+        {/* aution model --------------------------- */}
         {userType === "Auctioneer" && (
           <Modal show={showModal} onHide={handleModalClose} centered>
             <Modal.Header closeButton>
@@ -223,84 +165,6 @@ const Auction = () => {
             </Modal.Body>
           </Modal>
         )}
-        <Modal show={showBidderModal} onHide={handlebidderModalClose} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>Easy Bid</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group controlId="itemName">
-                <Form.Label>{bidderName}</Form.Label>
-              </Form.Group>
-              <Form.Group controlId="itemName">
-                <Form.Label>
-                  <b>{auctions.length > 0 && auctions[0].name}</b>
-                </Form.Label>
-              </Form.Group>
-              <Form.Group controlId="startingPrice" className="mb-3">
-                <Form.Label>Current Price</Form.Label>
-                {bidders.length === 0 ? (
-                  auctions.map((auction) => (
-                    <Row className="gx-0">
-                      <Col>
-                        <p>
-                          {auction.name} - ${auction.price}
-                        </p>
-                      </Col>
-                    </Row>
-                  ))
-                ) : (
-                  <Row className="gx-0">
-                    <Col>
-                      <p>
-                        {bidders[bidders.length - 1].name} - $
-                        {bidders[bidders.length - 1].amount}
-                      </p>
-                    </Col>
-                  </Row>
-                )}
-              </Form.Group>
-              <hr />
-
-              <Button
-                variant="primary"
-                type="submit"
-                onClick={(e) =>
-                  handlebidderModalSubmit(
-                    e,
-                    auctions.length > 0 && bidders.length === 0
-                      ? parseInt(auctions[0].price) + 100
-                      : bidders.length > 0 &&
-                          bidders[bidders.length - 1].amount + 100
-                  )
-                }
-                disabled={
-                  bidders.length > 0 &&
-                  bidders[bidders.length - 1].name === currentUser?.name
-                }
-              >
-                You Paid {bidAmount}
-              </Button>
-              <Button
-                variant="white"
-                onClick={() => {
-                  setBidAmount(parseInt(bidAmount) + 500);
-                }}
-              >
-                +500
-              </Button>
-              <Button
-                variant="white"
-                onClick={() => setBidAmount(parseInt(bidAmount) + 1000)}
-              >
-                +1000
-              </Button>
-              <Button variant="danger" onClick={() => console.log(bidAmount)}>
-                Reset
-              </Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
       </div>
     </Container>
   );
--- frontend/src/components/Bidder.js
+++ frontend/src/components/Bidder.js
@@ -1,23 +1,162 @@
-import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
-import { getAuction } from "../action"
-
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Card, Button, Form, Modal, Row, Col } from "react-bootstrap";
+import { createBid, getAuction, getBids, login } from "../action";
 const Bidder = () => {
-    const [auctions, setAuctions] = useState()
-    const dispatch = useDispatch()
+  const [auctions, setAuctions] = useState([]);
+  const [bidderName, setBidderName] = useState("");
+  const dispatch = useDispatch();
+  const [showBidderModal, setShowBidderModal] = useState(false);
+  const [bidders, setBidders] = useState([]);
+  const [bidAmount, setBidAmount] = useState();
+  const [userType, setUserType] = useState();
+  const currentUser = JSON.parse(localStorage.getItem("users"));
 
-    useEffect(() => {
-        async function getAuctionData() {
-            setAuctions(await dispatch(getAuction()))
-        }
-        getAuctionData()
-    }, [])
-    return (
-        <div>
-            <div>{console.log(auctions)}</div>
-        </div>
-    )
+  useEffect(() => {
+    async function getAuctionData() {
+      setAuctions(await dispatch(getAuction()));
+    }
+    getAuctionData();
+  }, []);
 
-}
+  useEffect(() => {
+    async function getAuctionData() {
+      setAuctions(await dispatch(getAuction()));
+      setBidders(await dispatch(getBids()));
+    }
+    getAuctionData();
+  }, []);
 
-export default Bidder
+  const handleBidderSubmit = (event) => {
+    event.preventDefault();
+    if (bidderName) {
+      setUserType("Bidder");
+      const data = {
+        name: bidderName,
+        userType: "Biddder",
+      };
+      dispatch(login(data));
+      setShowBidderModal(true);
+      setBidAmount(
+        auctions.length > 0 && bidders.length === 0
+          ? parseInt(auctions[0].price) + 100
+          : bidders.length > 0 && bidders[bidders.length - 1].amount + 100
+      );
+    }
+  };
+  const handlebidderModalSubmit = (event, bidPrice) => {
+    event.preventDefault();
+    const data = {
+      auction_id: auctions.length > 0 && auctions[0].id,
+      amount: bidPrice,
+      name: bidderName,
+    };
+    dispatch(createBid(data));
+  };
+  const handlebidderModalClose = () => {
+    setShowBidderModal(false);
+  };
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <Card.Title>Bidder</Card.Title>
+          <Form onSubmit={handleBidderSubmit}>
+            <Form.Group controlId="auctioneerName" className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={bidderName}
+                onChange={(event) => setBidderName(event.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <Modal show={showBidderModal} onHide={handlebidderModalClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Easy Bid</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="itemName">
+              <Form.Label>{bidderName}</Form.Label>
+            </Form.Group>
+            <Form.Group controlId="itemName">
+              <Form.Label>
+                <b>{auctions.length > 0 && auctions[0].name}</b>
+              </Form.Label>
+            </Form.Group>
+            <Form.Group controlId="startingPrice" className="mb-3">
+              <Form.Label>Current Price</Form.Label>
+              {bidders.length === 0 ? (
+                auctions.map((auction) => (
+                  <Row className="gx-0">
+                    <Col>
+                      <p>
+                        {auction.name} - ${auction.price}
+                      </p>
+                    </Col>
+                  </Row>
+                ))
+              ) : (
+                <Row className="gx-0">
+                  <Col>
+                    <p>
+                      {bidders[bidders.length - 1].name} - $
+                      {bidders[bidders.length - 1].amount}
+                    </p>
+                  </Col>
+                </Row>
+              )}
+            </Form.Group>
+            <hr />
+
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={(e) =>
+                handlebidderModalSubmit(
+                  e,
+                  auctions.length > 0 && bidders.length === 0
+                    ? parseInt(auctions[0].price) + 100
+                    : bidders.length > 0 &&
+                        bidders[bidders.length - 1].amount + 100
+                )
+              }
+              disabled={
+                bidders.length > 0 &&
+                bidders[bidders.length - 1].name === currentUser?.name
+              }
+            >
+              You Paid {bidAmount}
+            </Button>
+            <Button
+              variant="white"
+              onClick={() => {
+                setBidAmount(parseInt(bidAmount) + 500);
+              }}
+            >
+              +500
+            </Button>
+            <Button
+              variant="white"
+              onClick={() => setBidAmount(parseInt(bidAmount) + 1000)}
+            >
+              +1000
+            </Button>
+            <Button variant="danger" onClick={() => console.log(bidAmount)}>
+              Reset
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default Bidder;
