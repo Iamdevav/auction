8d65aa3f194c4ecae7d1d6fe73af2b7fa3872d07
Iamdevav <aviavipatel04@gmail.com>
2023-06-09 13:41:27 +0530

    add bidder model

--- frontend/src/components/Auction.js
+++ frontend/src/components/Auction.js
@@ -1,173 +1,222 @@
 import React, { useEffect, useState } from "react";
 import {
-    Card,
-    Button,
-    Form,
-    Modal,
-    Row,
-    Col,
-    Container,
+  Card,
+  Button,
+  Form,
+  Modal,
+  Row,
+  Col,
+  Container,
 } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { createAuction, login } from "../action";
 
 const Auction = () => {
-    const [auctioneerName, setAuctioneerName] = useState("");
-    const [bidderName, setBidderName] = useState("")
-    const [showModal, setShowModal] = useState(false);
-    const [itemName, setItemName] = useState("");
-    const [startingPrice, setStartingPrice] = useState("");
-    const [userType, setUserType] = useState()
+  const [auctioneerName, setAuctioneerName] = useState("");
+  const [bidderName, setBidderName] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [itemName, setItemName] = useState("");
+  const [startingPrice, setStartingPrice] = useState("");
+  const [userType, setUserType] = useState();
+  const [showBidderModal, setShowBidderModal] = useState(false);
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
+  useEffect(() => {}, []);
 
-    }, [])
+  const handleAuctioneerSubmit = (event) => {
+    event.preventDefault();
+    let data;
+    if (auctioneerName) {
+      setUserType("Auctioneer");
+      data = {
+        name: auctioneerName,
+        userType: "Auctioneer",
+      };
+    } else {
+      setUserType("Bidder");
+      data = {
+        name: bidderName,
+        userType: "Biddder",
+      };
+    }
+    dispatch(login(data));
+    setAuctioneerName("");
+    setBidderName("");
+    setShowModal(true);
+  };
 
-    const handleAuctioneerSubmit = (event) => {
-        event.preventDefault();
-        let data
-        if (auctioneerName) {
-            setUserType('Auctioneer')
-            data = {
-                name: auctioneerName,
-                userType: 'Auctioneer'
-            }
-
-        } else {
-            setUserType('Bidder')
-            data = {
-                name: bidderName,
-                userType: 'Biddder'
-            }
-        }
-        dispatch(login(data))
-        setAuctioneerName("")
-        setBidderName("")
-        setShowModal(true);
+  const handleModalSubmit = (event) => {
+    event.preventDefault();
+    console.log("Item Name:", itemName);
+    console.log("Starting Price:", startingPrice);
+    const data = {
+      name: itemName,
+      price: startingPrice,
     };
+    dispatch(createAuction(data));
 
-    const handleModalSubmit = (event) => {
-        event.preventDefault();
-        console.log("Item Name:", itemName);
-        console.log("Starting Price:", startingPrice);
-        // Do any additional logic here
-        const data = {
-            name: itemName,
-            price: startingPrice
-        }
-        dispatch(createAuction(data))
+    setItemName("");
+    setStartingPrice("");
+  };
 
-        // Clear the form fields if needed
-        setItemName("");
-        setStartingPrice("");
-    };
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+  const handleBidderSubmit = (event) => {
+    event.preventDefault();
+    setShowBidderModal(true);
+  };
+  const handlebidderModalSubmit = (event) => {
+    event.preventDefault();
+  };
+  const handlebidderModalClose = () => {
+    setShowBidderModal(false);
+  };
 
-    const handleModalClose = () => {
-        setShowModal(false);
-    };
+  return (
+    <Container>
+      <div>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Auctioneer</Card.Title>
+                <Form onSubmit={handleAuctioneerSubmit}>
+                  <Form.Group controlId="auctioneerName" className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      value={auctioneerName}
+                      onChange={(event) =>
+                        setAuctioneerName(event.target.value)
+                      }
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Bidder</Card.Title>
+                <Form onSubmit={handleBidderSubmit}>
+                  <Form.Group controlId="auctioneerName" className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      value={auctioneerName}
+                      onChange={(event) =>
+                        setAuctioneerName(event.target.value)
+                      }
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-    return (
-        <Container>
-            <div>
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Auctioneer</Card.Title>
-                                <Form onSubmit={handleAuctioneerSubmit}>
-                                    <Form.Group controlId="auctioneerName" className="mb-3">
-                                        <Form.Label>Name</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Enter name"
-                                            value={auctioneerName}
-                                            onChange={(event) =>
-                                                setAuctioneerName(event.target.value)
-                                            }
-                                        />
-                                    </Form.Group>
-                                    <Button variant="primary" type="submit">
-                                        Submit
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Bidder</Card.Title>
-                                <Form onSubmit={handleAuctioneerSubmit}>
-                                    <Form.Group controlId="bidderName" className="mb-3">
-                                        <Form.Label>Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter name" value={bidderName} onChange={(event) => setBidderName(event.target.value)} />
-                                    </Form.Group>
-                                    <Button variant="primary" type="submit">
-                                        Submit
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+        {userType === "Auctioneer" && (
+          <Modal
+            show={showModal}
+            onHide={handleModalClose}
+            //   style={{ height: "500vh" }}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Enter Item Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group controlId="itemName">
+                  <Form.Label>Auction Name</Form.Label>
+                </Form.Group>
+                <Form.Group controlId="itemName">
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter item name"
+                    value={itemName}
+                    onChange={(event) => setItemName(event.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group controlId="startingPrice" className="mb-3">
+                  <Form.Label>Starting Price</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter starting price"
+                    value={startingPrice}
+                    onChange={(event) => setStartingPrice(event.target.value)}
+                  />
+                </Form.Group>
 
-                {userType === "Auctioneer" && <Modal
-                    show={showModal}
-                    onHide={handleModalClose}
-                //   style={{ height: "500vh" }}
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={handleModalSubmit}
                 >
-                    <Modal.Header closeButton>
-                        <Modal.Title>Enter Item Details</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Group controlId="itemName">
-                                <Form.Label>Auction Name</Form.Label>
-                            </Form.Group>
-                            <Form.Group controlId="itemName">
-                                <Form.Label>Item Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter item name"
-                                    value={itemName}
-                                    onChange={(event) => setItemName(event.target.value)}
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="startingPrice" className="mb-3">
-                                <Form.Label>Starting Price</Form.Label>
-                                <Form.Control
-                                    type="number"
-                                    placeholder="Enter starting price"
-                                    value={startingPrice}
-                                    onChange={(event) => setStartingPrice(event.target.value)}
-                                />
-                            </Form.Group>
+                  Submit
+                </Button>
+                <hr />
+                <p> current price</p>
+                <Row className="gx-0">
+                  <Col>
+                    <p>Bidder Name:</p>
+                  </Col>
+                  <Col>
+                    <p>$XXX</p>{" "}
+                  </Col>
+                </Row>
+              </Form>
+            </Modal.Body>
+          </Modal>
+        )}
+        <Modal
+          show={showBidderModal}
+          onHide={handlebidderModalClose}
+          //   style={{ height: "500vh" }}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Enter Item Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="itemName">
+                <Form.Label>bidder Name</Form.Label>
+              </Form.Group>
+              <hr />
+              <Form.Group controlId="itemName">
+                <Form.Label>Item Name</Form.Label>
+              </Form.Group>
+              <hr />
+              <Form.Group controlId="startingPrice" className="mb-3">
+                <Form.Label>Current Price</Form.Label>
+              </Form.Group>
+              <hr />
 
-                            <Button
-                                variant="primary"
-                                type="submit"
-                                onClick={handleModalSubmit}
-                            >
-                                Submit
-                            </Button>
-                            <hr />
-                            <p> current price</p>
-                            <Row className="gx-0">
-                                <Col>
-                                    <p>Bidder Name:</p>
-                                </Col>
-                                <Col>
-                                    <p>$XXX</p> {/* Replace XXX with the actual current price */}
-                                </Col>
-                            </Row>
-                        </Form>
-                    </Modal.Body>
-                </Modal>}
-            </div>
-        </Container>
-    );
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={handlebidderModalSubmit}
+              >
+                You Paid
+              </Button>
+              <hr />
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
+    </Container>
+  );
 };
 
 export default Auction;
